a9fd03de36d7c9f7373eaaccb9f6eeab1fd4405f
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-08-30 00:26:46 +0200

    Added an initial main trainer body

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Footer } from './Components/Footer'
 import { Header } from './Components/Header'
+import Trainer from './Components/Trainer'
 
 export default function App () {
   return (
     <div className="max-w-screen-xl space-y-4 mx-auto my-0 flex flex-col justify-center">
       <Header />
+      <Trainer />
       <Footer />
     </div>
   )
---
+++ src/Components/Trainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Trainer () {
+  return (
+    <div className="bg-white p-4">
+      <h1 className="text-4xl font-mono">NF-Trainer</h1>
+    </div>
+  )
+}
