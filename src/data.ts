e6aaad360c312f355c47d6cab7fe5c05c25e3898
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-08-30 20:29:37 +0200

    Added initial application data

---
+++ src/data.ts
@@ -0,0 +1,18 @@
+export default [
+  {
+    id: 1,
+    description: 'Dummy description 1'
+  },
+  {
+    id: 2,
+    description: 'Dummy description 2'
+  },
+  {
+    id: 3,
+    description: 'Dummy description 3'
+  },
+  {
+    id: 4,
+    description: 'Dummy description 4'
+  }
+]
