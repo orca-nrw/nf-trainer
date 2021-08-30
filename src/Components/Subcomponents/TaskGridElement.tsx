36012d1af9268e58900b17cc24ee52926548f60f
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-08-30 20:31:53 +0200

    Replaced taskBlock with TaskGridElement

--- src/Components/Subcomponents/TaskGridElement.tsx
+++ src/Components/Subcomponents/TaskGridElement.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Task } from '../../types/task'
 
-export default function TaskBlock ({ taskId }: PropTypes) {
+export default function TaskGridElement ({ task }: PropTypes) {
   return (
     <div className="bg-gray-300 border border-gray-400 rounded-sm">
-      <h1>Task {taskId}</h1>
-      <p>Content content content</p>
-      <Link to={`/tasks/${taskId}/firstNormalForm`}>
+      <h1>Task {task.id}</h1>
+      <p>
+        {task.description}
+      </p>
+      <Link to={`/tasks/${task.id}/firstNormalForm`}>
         <button type="button" className="px-2 py-1 bg-purple-500 hover:bg-purple-600 text-white text-lg rounded-md cursor-pointer">Begin</button>
       </Link>
     </div>
@@ -14,5 +17,5 @@ export default function TaskBlock ({ taskId }: PropTypes) {
 }
 
 interface PropTypes {
-  taskId: string
+  task: Task
 }
