83cb27550f8c5a177e48422c716f6098a301089f
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-09-01 02:01:48 +0200

    Changed styling

--- src/Components/Subcomponents/TaskGridElement.tsx
+++ src/Components/Subcomponents/TaskGridElement.tsx
@@ -4,13 +4,13 @@ import { Task } from '../../types/task'
 
 export default function TaskGridElement ({ task }: PropTypes) {
   return (
-    <div className="bg-gray-300 border border-gray-400 rounded-sm">
-      <h1>Task {task.id}</h1>
+    <div className="p-4 bg-gray-300 border border-gray-400 rounded-sm">
+      <h1 className="font-bold">Aufgabe {task.id}</h1>
       <p>
         {task.description}
       </p>
       <Link to={`/tasks/${task.id}/firstNormalForm`}>
-        <button type="button" className="px-2 py-1 bg-purple-500 hover:bg-purple-600 text-white text-lg rounded-md cursor-pointer">Begin</button>
+        <button type="button" className="px-2 py-1 bg-pink-600 hover:bg-pink-500 text-white text-lg rounded-md cursor-pointer">Begin</button>
       </Link>
     </div>
   )
