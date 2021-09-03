1e304ee09d876f44c6ceadaa478eea0a5796d3ea
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-09-03 18:52:54 +0200

    Added responsehandler

--- src/Components/TrainerPages/FirstNormalForm.tsx
+++ src/Components/TrainerPages/FirstNormalForm.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Redirect, useParams } from 'react-router-dom'
 import tasks from '../../data'
+import BooleanResponseHandler from '../Subcomponents/BooleanResponseHandler'
 import Table from '../Subcomponents/Table'
 
 export default function FirstNormalForm () {
@@ -13,9 +14,13 @@ export default function FirstNormalForm () {
 
   return (
     <>
+    <div className="space-y-4">
       <h1 className="font-bold text-xl">Erste Normalform</h1>
       <p>{task.description}</p>
       <Table tableData={task.tableData}/>
+      <p className="text-center">Befindet sich die Tabelle in der ersten Normalform?</p>
+      <BooleanResponseHandler isCorrect={true} redirectTo="/" />
+    </div>
     </>
   )
 }
