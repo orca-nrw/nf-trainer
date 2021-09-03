3f168ba93749d55ccc6f7c3f67352f1d709b64e7
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-09-03 17:28:34 +0200

    Removed subcomponent routing

--- src/Components/Trainer.tsx
+++ src/Components/Trainer.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Switch, Route, Link } from 'react-router-dom'
 import Home from './TrainerPages/Home'
-import Tasks from './TrainerPages/Tasks'
+import TaskSelection from './TrainerPages/TaskSelection'
+import FirstNormalForm from './TrainerPages/FirstNormalForm'
 
 export default function Trainer () {
   return (
@@ -12,13 +13,34 @@ export default function Trainer () {
         </Link>
       </div>
       <div className="bg-white p-4">
-        <Switch>
-          <Route exact path="/">
+      <Switch>
+        <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/tasks">
-            <Tasks />
+          <Route exact path="/tasks">
+            <TaskSelection />
           </Route>
+          <Route path="/tasks/:id/firstNormalForm">
+            <FirstNormalForm />
+          </Route>
+          {/* <Route path="/violatingColumns">
+            <ViolatingColumns />
+          </Route>
+          <Route path="/functionalDependencies">
+            <FunctionalDependencies />
+          </Route>
+          <Route path="/primaryKeys">
+            <PrimaryKeys />
+          </Route>
+          <Route path="/functionalDependencyTypes">
+            <FunctionalDependencyTypes />
+          </Route>
+          <Route path="/secondNormalForm">
+            <SecondNormalForm />
+          </Route>
+          <Route path="/thirdNormalForm">
+            <ThirdNormalForm />
+          </Route> */}
         </Switch>
       </div>
     </>
