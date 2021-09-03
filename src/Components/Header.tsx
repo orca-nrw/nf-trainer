71cf330d9dc1f0b4caa9694e5776926ca89137c4
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-09-03 19:31:59 +0200

    Added link to head

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import edbHeaderLogo from '../img/edb_header_logo.png'
 import eildHeaderLogo from '../img/eild_header_logo.png'
 
@@ -6,7 +7,9 @@ export const Header = () => {
   return (
     <header className="bg-white p-4">
       <div className="w-full flex justify-between">
-        <img className="max-h-32" src={edbHeaderLogo} alt="EDB Logo" />
+        <Link to="/">
+          <img className="max-h-32" src={edbHeaderLogo} alt="EDB Logo" />
+        </Link>
         <img className="max-h-32" src={eildHeaderLogo} alt="EILD Logo" />
       </div>
     </header>
