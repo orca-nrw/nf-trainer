843d91b574045aed21f70f4f9ceeb949b5e22dd6
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-09-06 01:19:54 +0200

    Created keySelection Component

---
+++ src/Components/Subcomponents/KeySelectionContainer.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import CheckboxResponseHandler from './CheckboxResponseHandler'
+
+interface PropTypes {
+  keys: string[]
+}
+
+export default function KeySelectionContainer ({ keys }: PropTypes) {
+  const [selectedPrimaryKeys, setSelectedPrimaryKeys] = useState<string[]>([])
+  const [selectedColumns, setSelectedColumns] = useState<string[]>([])
+
+  return (
+    <div className="grid grid-cols-2 gap-4 border p-4 shadow-md">
+      <div>
+        <h1 className="font-bold">Primärschlüssel</h1>
+        <CheckboxResponseHandler entryList={keys} selectedEntries={selectedPrimaryKeys} setSelectedEntries={setSelectedPrimaryKeys} selectionColor="purple" />
+      </div>
+      <div>
+        <h1 className="font-bold">Spalten</h1>
+        <CheckboxResponseHandler entryList={keys} selectedEntries={selectedColumns} setSelectedEntries={setSelectedColumns} selectionColor="red" />
+      </div>
+    </div>
+  )
+}
