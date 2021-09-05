df94e897aa3f9a8f21b1a1bcca2eba0f58a56eba
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-09-06 01:21:53 +0200

    Added keySelectionContainer

--- src/Components/TrainerPages/FunctionalDependencies.tsx
+++ src/Components/TrainerPages/FunctionalDependencies.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import tasks from '../../data'
 import { Redirect, useParams } from 'react-router-dom'
 import Table from '../Subcomponents/Table'
+import KeySelectionContainer from '../Subcomponents/KeySelectionContainer'
 
 export default function FunctionalDependencies () {
   // Get task from url param
@@ -11,12 +12,21 @@ export default function FunctionalDependencies () {
   // Redirect to index if there is no task with the given id
   if (!task) return <Redirect to="/" />
 
+  const taskKeys = Object.keys(task.tableData[0])
+
   return (
-    <div className="space-y-4">
+    <div className="flex flex-col items-center space-y-4">
       <h1 className="font-bold text-xl">Funktionale Abhängigkeiten</h1>
       <p>{task.description}</p>
       <Table tableData={task.tableData}/>
       <p className="text-center">Bestimmen Sie alle funktionalen Abhängigkeiten!</p>
+      <div className="grid grid-cols-2 gap-8">
+        <KeySelectionContainer keys={taskKeys} />
+        <KeySelectionContainer keys={taskKeys} />
+        <KeySelectionContainer keys={taskKeys} />
+        <KeySelectionContainer keys={taskKeys} />
+      </div>
+      <button className="px-4 py-2 bg-red-500 hover:bg-red-600 text-white text-lg font-semibold border shadow-md rounded-md cursor-pointer">Auswerten</button>
     </div>
   )
 }
