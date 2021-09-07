b6b7e2410f876fa12d4a1f3c216b9993b23a2efb
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-09-07 02:32:48 +0200

    Lifted state up

--- src/Components/Subcomponents/KeySelectionContainer.tsx
+++ src/Components/Subcomponents/KeySelectionContainer.tsx
@@ -1,23 +1,41 @@
-import React, { useState } from 'react'
+import React from 'react'
 import CheckboxResponseHandler from './CheckboxResponseHandler'
 
-interface PropTypes {
-  keys: string[]
+interface Association {
+  primaryKeys: string[],
+  columns: string[]
 }
 
-export default function KeySelectionContainer ({ keys }: PropTypes) {
-  const [selectedPrimaryKeys, setSelectedPrimaryKeys] = useState<string[]>([])
-  const [selectedColumns, setSelectedColumns] = useState<string[]>([])
+interface PropTypes {
+  keys: string[],
+  association: Association,
+  updateAssociation: (x: Association) => void
+}
+
+export default function KeySelectionContainer ({ keys, association, updateAssociation }: PropTypes) {
+  function updatePrimaryKeys (updatedPrimaryKeys: string[]) {
+    updateAssociation({
+      primaryKeys: updatedPrimaryKeys,
+      columns: association.columns
+    })
+  }
+
+  function updateColumns (updatedColumns: string[]) {
+    updateAssociation({
+      primaryKeys: association.primaryKeys,
+      columns: updatedColumns
+    })
+  }
 
   return (
     <div className="grid grid-cols-2 gap-4 border p-4 shadow-md">
       <div>
         <h1 className="font-bold">Primärschlüssel</h1>
-        <CheckboxResponseHandler entryList={keys} selectedEntries={selectedPrimaryKeys} setSelectedEntries={setSelectedPrimaryKeys} useAccent={true} />
+        <CheckboxResponseHandler entryList={keys} selectedEntries={association.primaryKeys} setSelectedEntries={updatePrimaryKeys} useAccent={true} />
       </div>
       <div>
         <h1 className="font-bold">Spalten</h1>
-        <CheckboxResponseHandler entryList={keys} selectedEntries={selectedColumns} setSelectedEntries={setSelectedColumns} useAccent={false} />
+        <CheckboxResponseHandler entryList={keys} selectedEntries={association.columns} setSelectedEntries={updateColumns} useAccent={false} />
       </div>
     </div>
   )
