351f28a80b416d438ca5201480710e91a9e49034
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-09-07 17:32:41 +0200

    Styling

--- src/Components/TrainerPages/TaskSelection.tsx
+++ src/Components/TrainerPages/TaskSelection.tsx
@@ -4,8 +4,8 @@ import tasks from '../../data'
 
 export default function TaskSelection () {
   return (
-    <div>
-      <h1>Task Selection</h1>
+    <div className="space-y-4">
+      <h1 className="font-bold text-xl">Task Selection</h1>
       <div className="grid grid-cols-2 gap-4">
         {tasks.map(task => {
           return <TaskGridElement key={task.id} task={task} />
