2cd4774dca2cf864cb53f5b3780bbfb062e97804
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-09-09 02:16:48 +0200

    Added dependencyType component content

--- src/Components/Subcomponents/FunctionalDependencyHandler.tsx
+++ src/Components/Subcomponents/FunctionalDependencyHandler.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import uniqueId from '../../utils/uniqueId'
 
 interface FunctionalDependency {
   primaryKeys: string[],
@@ -7,13 +8,25 @@ interface FunctionalDependency {
 }
 
 interface PropTypes {
-  functionalDependencies:FunctionalDependency[]
+  functionalDependencies: FunctionalDependency[]
 }
 
 export default function FunctionalDependencyHandler ({ functionalDependencies }: PropTypes) {
   return (
-    <div>
-
+    <div className="flex flex-col space-y-4">
+      {functionalDependencies.map(dependency => {
+        const dependencyString = `${dependency.primaryKeys.join(', ')} -> ${dependency.columns.join(', ')}`
+        return (
+          <div className="text-right" key={uniqueId()}>
+            <label className="mr-4 font-semibold">{dependencyString}</label>
+            <select className="border rounded p-1">
+              <option value="voll">Voll</option>
+              <option value="partiell">Partiell</option>
+              <option value="transitiv">Transitiv</option>
+            </select>
+          </div>
+        )
+      })}
     </div>
   )
 }
