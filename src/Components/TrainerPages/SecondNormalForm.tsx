084350facf89d7d26d0782f78c31bf3f2f724653
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-09-21 01:00:40 +0200

    Added 2nf page

---
+++ src/Components/TrainerPages/SecondNormalForm.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Redirect, useParams } from 'react-router'
+import tasks from '../../data'
+import AssociationResponseHandler from '../Subcomponents/AssociationResponseHandler'
+import Table from '../Subcomponents/Table'
+
+interface ParamTypes {
+  id: string
+}
+
+export default function SecondNormalForm () {
+  // Get task from url param
+  const { id } = useParams<ParamTypes>()
+  const task = tasks.find(task => task.id === Number(id))
+
+  // Redirect to index if there is no task with the given id
+  if (!task) return <Redirect to="/" />
+
+  // Task Variables
+  const taskKeys = Object.keys(task.tableData[0])
+  const associations = task.secondNormalForm
+
+  return (
+    <div className="space-y-4">
+      <h1 className="font-bold text-xl">Zweite Normalform</h1>
+      <p>{task.description}</p>
+      <Table tableData={task.tableData} />
+      <p className="text-center">Bringen Sie das Schema in die zweite Normalform!</p>
+      <AssociationResponseHandler keys={taskKeys} associationsSolutions={associations} redirectTo={`/task/${id}/thirdNormalForm`} />
+    </div>
+  )
+}
