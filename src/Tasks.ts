47e6b81e7732dc9d7f442422b2592ef38cea1930
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-09-30 02:25:44 +0200

    Added Aufgabe 2

--- src/Tasks.ts
+++ src/Tasks.ts
@@ -190,6 +190,224 @@ const tasks: Task[] = [
         columns: ['Anzahl']
       }
     ]
+  },
+  {
+    id: 2,
+    description: `Betrachten Sie die folgende, in der Arztpraxis von Dr. Beschränkt geführte Tabelle, wobei KK für Krankenkasse und P für Patient steht. 
+    Ein Patient gehört genau einer Krankenkasse an und hat an einem Behandlungstag zu einer Diagnose höchsten eine Therapie.`,
+    tableData: [
+      {
+        KK_NR: 1467,
+        KK_Name: 'AOK-GM',
+        P_Name: 'Müller, Erwin',
+        P_Adresse: 'GM',
+        P_Geb_Tag: '13.08.56',
+        Behandlungstag: '04.04.02; 31.03.02',
+        Diagnose: 'Hautrötung; Abschürfung; Sodbrennen; Tennisarm',
+        Therapie: 'Salbe; Pflaster; Tabletten; Spritze',
+        KK_Adresse: 'GM',
+        Diagnose_Beschreibung: 'Haut ist rot, Teile der Haut sind abgeschürft, Patient hat Sodbrennen, Patient hat einen Tennisarm',
+        P_Tel: '02261/45556'
+      },
+      {
+        KK_NR: 1789,
+        KK_Name: 'BKK-Textil',
+        P_Name: 'Meier, Ema',
+        P_Adresse: 'Berg-Gladbach',
+        P_Geb_Tag: '12.11.67',
+        Behandlungstag: '31.05.02',
+        Diagnose: 'Sodbrennen',
+        Therapie: 'Tabletten',
+        KK_Adresse: 'Köln',
+        Diagnose_Beschreibung: 'Patient hat Sodbrennen',
+        P_Tel: '02263/344785'
+      },
+      {
+        KK_NR: 3456,
+        KK_Name: 'TK',
+        P_Name: 'Bäcker, Arno',
+        P_Adresse: 'GM',
+        P_Geb_Tag: '30.04.80',
+        Behandlungstag: '12.09.02',
+        Diagnose: 'Abschürfung',
+        Therapie: 'Pflaster',
+        KK_Adresse: 'Köln',
+        Diagnose_Beschreibung: 'Teile der Haut sind abgeschürft',
+        P_Tel: '02261/643678'
+      },
+      {
+        KK_NR: 3456,
+        KK_Name: 'TK',
+        P_Name: 'Schulz, Hugo',
+        P_Adresse: 'Köln',
+        P_Geb_Tag: '30.06.79',
+        Behandlungstag: '08.09.99',
+        Diagnose: 'Tennisarm',
+        Therapie: 'Spritze',
+        KK_Adresse: 'Köln',
+        Diagnose_Beschreibung: 'Patient hat einen Tennisarm',
+        P_Tel: '0221/5477428'
+      }
+    ],
+    hasViolatingColumns: true,
+    violatingColumns: ['Behandlungstag', 'Diagnose_Beschreibung', 'Diagnose', 'Therapie'],
+    functionalDependencies: [
+      {
+        primaryKeys: ['KK_NR'],
+        columns: ['KK_Name', 'KK_Adresse'],
+        type: 'transitiv'
+      },
+      {
+        primaryKeys: ['P_Name'],
+        columns: ['KK_NR', 'KK_Name', 'P_Adresse', 'P_Geb_Tag', 'KK_Adresse', 'P_Tel'],
+        type: 'partiell'
+      },
+      {
+        primaryKeys: ['P_Name', 'Behandlungstag', 'Diagnose'],
+        columns: ['Therapie'],
+        type: 'voll'
+      },
+      {
+        primaryKeys: ['Diagnose'],
+        columns: ['Diagnose_Beschreibung'],
+        type: 'partiell'
+      }
+    ],
+    primaryKeys: ['P_Name', 'Behandlungstag', 'Diagnose'],
+    secondNormalForm: [
+      {
+        primaryKeys: ['P_Name'],
+        columns: ['KK_NR', 'KK_Name', 'P_Adresse', 'P_Geb_Tag', 'KK_Adresse', 'P_Tel']
+      },
+      {
+        primaryKeys: ['P_Name', 'Behandlungstag', 'Diagnose'],
+        columns: ['Therapie']
+      },
+      {
+        primaryKeys: ['Diagnose'],
+        columns: ['Diagnose_Beschreibung']
+      }
+    ],
+    secondFormTableData: [
+      [
+        {
+          P_Name: 'Müller, Erwin',
+          KK_NR: '1467',
+          KK_Name: 'AOK-GM',
+          P_Adresse: 'GM',
+          P_Geb_Tag: '13.08.56',
+          KK_Adresse: 'GM',
+          P_Tel: '02261/45556'
+        },
+        {
+          P_Name: 'Meier, Ema',
+          KK_NR: '1789',
+          KK_Name: 'BKK-Textil',
+          P_Adresse: 'Berg.-Gladb.',
+          P_Geb_Tag: '12.11.67',
+          KK_Adresse: 'Köln',
+          P_Tel: '02263/344785'
+        },
+        {
+          P_Name: 'Bäcker, Arno',
+          KK_NR: '3456',
+          KK_Name: 'TK',
+          P_Adresse: 'GM',
+          P_Geb_Tag: '30.04.80',
+          KK_Adresse: 'Köln',
+          P_Tel: '02261/643678'
+        },
+        {
+          P_Name: 'Schulz, Hugo',
+          KK_NR: '3456',
+          KK_Name: 'TK',
+          P_Adresse: 'Köln',
+          P_Geb_Tag: '30.06.79',
+          KK_Adresse: 'Köln',
+          P_Tel: '0221/5477428'
+        }
+      ],
+      [
+        {
+          P_Name: 'Meier, Ema',
+          Behandlungstag: '31.05.98',
+          Diagnose: 'Sodbrennen',
+          Therapie: 'Tabletten'
+        },
+        {
+          P_Name: 'Müller, Erwin',
+          Behandlungstag: '31.03.99',
+          Diagnose: 'Tennisarm',
+          Therapie: 'Spritze'
+        },
+        {
+          P_Name: 'Müller, Erwin',
+          Behandlungstag: '04.04.98',
+          Diagnose: 'Hautrötung',
+          Therapie: 'Salbe'
+        },
+        {
+          P_Name: 'Müller, Erwin',
+          Behandlungstag: '04.04.98',
+          Diagnose: 'Abschürfung',
+          Therapie: 'Pflaster'
+        },
+        {
+          P_Name: 'Müller, Erwin',
+          Behandlungstag: '04.04.98',
+          Diagnose: 'Sodbrennen',
+          Therapie: 'Tabletten'
+        },
+        {
+          P_Name: 'Bäcker, Arno',
+          Behandlungstag: '12.09.99',
+          Diagnose: 'Abschürfung',
+          Therapie: 'Pflaster'
+        },
+        {
+          P_Name: 'Schulz, Hugo',
+          Behandlungstag: '08.09.99',
+          Diagnose: 'Tennisarm',
+          Therapie: 'Spritze'
+        }
+      ],
+      [
+        {
+          Diagnose: 'Abschürfung',
+          Diagnose_Beschreibung: 'Teile der Haut sind abgeschürft'
+        },
+        {
+          Diagnose: 'Tennisarm',
+          Diagnose_Beschreibung: 'Patient hat einen Tennisarm'
+        },
+        {
+          Diagnose: 'Sodbrennen',
+          Diagnose_Beschreibung: 'Patient hat Sodbrennen'
+        },
+        {
+          Diagnose: 'Hautrötung',
+          Diagnose_Beschreibung: 'Haut ist rot'
+        }
+      ]
+    ],
+    thirdNormalFormSolutions: [
+      {
+        primaryKeys: ['P_Name'],
+        columns: ['P_Adresse', 'P_Geb_Tag', 'P_Tel', 'KK_NR']
+      },
+      {
+        primaryKeys: ['KK_NR'],
+        columns: ['KK_Name', 'KK_Adresse']
+      },
+      {
+        primaryKeys: ['P_Name', 'Behandlungstag', 'Diagnose'],
+        columns: ['Therapie']
+      },
+      {
+        primaryKeys: ['Diagnose'],
+        columns: ['Diagnose_Beschreibung']
+      }
+    ]
   }
 ]
 
