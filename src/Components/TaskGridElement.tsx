b98a6c50530da1898715b5de1c0d65d6fd9b21bc
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-10-04 14:37:01 +0200

    Taskgrid now displays titles

--- src/Components/TaskGridElement.tsx
+++ src/Components/TaskGridElement.tsx
@@ -6,7 +6,7 @@ export default function TaskGridElement ({ task }: PropTypes) {
     <div className="p-4 bg-gray-300 border border-gray-400 rounded-sm">
       <h1 className="font-bold">Aufgabe {task.id}</h1>
       <p>
-        {task.description}
+        {task.title}
       </p>
       <Link to={`/tasks/${task.id}/firstNormalForm`}>
         <button type="button" className="px-2 py-1 bg-red-500 hover:bg-red-600 text-white text-lg rounded-md cursor-pointer">Begin</button>
@@ -18,6 +18,6 @@ export default function TaskGridElement ({ task }: PropTypes) {
 interface PropTypes {
   task: {
     id: number,
-    description: string
+    title: string
   }
 }
