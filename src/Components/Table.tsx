1471ad6a031760b0768d4c25139dcabeedd4ad64
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-10-09 20:27:11 +0200

    Fixed overflow

--- src/Components/Table.tsx
+++ src/Components/Table.tsx
@@ -2,26 +2,28 @@ import React from 'react'
 
 export default function Table ({ tableData }: PropType) {
   return (
-    <table className="shadow w-full">
-      <thead>
-        <tr className="border">
-          {Object.keys(tableData[0]).map(key => {
-            return <th className="bg-gray-100 border text-left px-4 py-2" key={key}>{key}</th>
+    <div className="overflow-x-auto">
+      <table className="shadow  w-full">
+        <thead>
+          <tr className="border">
+            {Object.keys(tableData[0]).map(key => {
+              return <th className="bg-gray-100 border text-left px-4 py-2" key={key}>{key}</th>
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((row, index) => {
+            return (
+              <tr className={index % 2 === 1 ? 'bg-gray-50' : ''} key={index}>
+                {Object.values(row).map((entry, index) => {
+                  return <td className="border px-4 py-2" key={index}>{entry}</td>
+                })}
+              </tr>
+            )
           })}
-        </tr>
-      </thead>
-      <tbody>
-        {tableData.map((row, index) => {
-          return (
-            <tr className={index % 2 === 1 ? 'bg-gray-50' : ''} key={index}>
-              {Object.values(row).map((entry, index) => {
-                return <td className="border px-4 py-2" key={index}>{entry}</td>
-              })}
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
