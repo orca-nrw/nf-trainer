681437f9f6bdb2a515ffc10c08f2193043b5fbde
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-10-09 20:30:24 +0200

    Fixed unnecessary grid

--- src/Components/TableGrid.tsx
+++ src/Components/TableGrid.tsx
@@ -6,11 +6,17 @@ interface PropTypes {
 }
 
 export default function TableGrid ({ gridData }: PropTypes) {
-  return (
+  if (gridData.length > 1) {
+    return (
     <div className="grid grid-cols-2 gap-4">
       {gridData.map((tableData, index) => {
         return <Table tableData={tableData} key={index} />
       })}
     </div>
-  )
+    )
+  } else {
+    return (
+      <Table tableData={gridData[0]} />
+    )
+  }
 }
