063fecc1b9f8a017368e4b8eca632669bd8e2ac0
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-10-09 22:47:21 +0200

    Added th-colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'th-red': '#c81e0f',
+        'th-orange': '#ea5a00',
+        'th-violet': '#b43092'
+      }
+    }
   },
   variants: {
     extend: {}
