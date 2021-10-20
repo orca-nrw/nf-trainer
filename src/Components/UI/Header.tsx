8d217b93095fa121de45d2600348c92e4614e9de
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-10-20 23:22:36 +0200

    Improved responsiveness

--- src/Components/UI/Header.tsx
+++ src/Components/UI/Header.tsx
@@ -6,12 +6,16 @@ import eildHeaderLogo from '../../img/eild_header_logo.png'
 export const Header = () => {
   return (
     <header className="bg-white p-4">
-      <div className="w-full flex justify-between">
-        <Link to="/">
-          <img className="max-h-32" src={edbHeaderLogo} alt="EDB Logo" />
-        </Link>
-        <img className="max-h-32" src={eildHeaderLogo} alt="EILD Logo" />
-      </div>
+      <Link to="/">
+        <div className="w-full flex">
+          <div className="flex-auto">
+            <img className="max-h-32 max-w-full" src={edbHeaderLogo} alt="EDB Logo" />
+          </div>
+          <div className="flex-auto">
+            <img className="max-h-32 max-w-full float-right" src={eildHeaderLogo} alt="EILD Logo" />
+          </div>
+        </div>
+      </Link>
     </header>
   )
 }
