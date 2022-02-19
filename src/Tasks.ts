8e1b5d8b539f03c79c91c8b3f1c684c4e1c4363f
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2022-02-19 18:24:24 +0100

    Extracted Task Interface

--- src/Tasks.ts
+++ src/Tasks.ts
@@ -1,27 +1,4 @@
-interface Task {
-  id: number,
-  title: string,
-  description: string,
-  tableData: Object[],
-  hasViolatingColumns: boolean,
-  violatingColumns: string[],
-  functionalDependencies: {
-    primaryKeys: string[],
-    columns: string[],
-    type: string
-  }[],
-  primaryKeys: string[],
-  secondNormalForm: {
-    primaryKeys: string[],
-    columns: string[]
-  }[],
-  secondFormTableData: Object[][],
-  alreadyThirdNormalForm: boolean,
-  thirdNormalFormSolutions: {
-    primaryKeys: string[],
-    columns: string[]
-  }[]
-}
+import { Task } from './Types/Task'
 
 const tasks: Task[] = [
   {
---
+++ src/Types/Task.ts
@@ -0,0 +1,24 @@
+export interface Task {
+  id: number,
+  title: string,
+  description: string,
+  tableData: Object[],
+  hasViolatingColumns: boolean,
+  violatingColumns: string[],
+  functionalDependencies: {
+    primaryKeys: string[],
+    columns: string[],
+    type: string
+  }[],
+  primaryKeys: string[],
+  secondNormalForm: {
+    primaryKeys: string[],
+    columns: string[]
+  }[],
+  secondFormTableData: Object[][],
+  alreadyThirdNormalForm: boolean,
+  thirdNormalFormSolutions: {
+    primaryKeys: string[],
+    columns: string[]
+  }[]
+}
