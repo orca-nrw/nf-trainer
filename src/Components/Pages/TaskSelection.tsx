07efff7f0918d56f70ee0034708f39e7a117ccc5
Alexander Kosmehl <kosmehl@live.de>
2022-04-19 15:12:35 +0200

    now hides explanation modal

--- src/Components/Pages/TaskSelection.tsx
+++ src/Components/Pages/TaskSelection.tsx
@@ -6,7 +6,7 @@ import tasks from '../../Tasks'
 export default function TaskSelection () {
   return (
     <div className="space-y-8 mb-8">
-      <TrainerHeader>Aufgabenauswahl</TrainerHeader>
+      <TrainerHeader hideExplanationsModal={true} >Aufgabenauswahl</TrainerHeader>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4 pt-4">
         {tasks.map(task => {
           return <TaskGridElement key={task.id} task={task} />
