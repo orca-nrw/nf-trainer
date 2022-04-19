870a1e2e64a414c29e814bd0c6ff4495472d245c
Alexander Kosmehl <kosmehl@live.de>
2022-04-19 15:12:19 +0200

    Added explanation modal button

--- src/Components/UI/TrainerHeader.tsx
+++ src/Components/UI/TrainerHeader.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import ExplanationsModalButton from './ExplanationsModalButton'
 
 interface PropTypes {
   children?: React.ReactNode
+  hideExplanationsModal?: boolean
 }
 
-export default function TrainerHeader ({ children }: PropTypes) {
+export default function TrainerHeader ({ children, hideExplanationsModal = false }: PropTypes) {
   return (
-    <h1 className="font-bold text-2xl">
-      {children}
-    </h1>
+    <div className="flex flex-row justify-between">
+      <h1 className="font-bold text-2xl">{children}</h1>
+      { !hideExplanationsModal &&
+        <ExplanationsModalButton />
+      }
+    </div>
   )
 }
