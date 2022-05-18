141c980cde1bb8e02eafc73617f31f995078860a
Alexander Kosmehl <kosmehl@live.de>
2022-05-19 01:05:51 +0200

    Updated react-scripts & tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         'th-red': '#c81e0f',
         'th-orange': '#ea5a00',
-        'th-violet': '#b43092'
-      }
-    }
+        'th-violet': '#b43092',
+      },
+    },
   },
   variants: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 }
