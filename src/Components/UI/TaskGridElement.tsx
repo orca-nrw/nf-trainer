2fed4567a6c318a7f8c92aafc54f9186d97627fc
Alexander Kosmehl <kosmehl@live.de>
2022-07-07 16:18:40 +0200

    Updated task grid to work with new task list

--- src/Components/UI/TaskGridElement.tsx
+++ src/Components/UI/TaskGridElement.tsx
@@ -1,26 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
-export default function TaskGridElement({ task }: PropTypes) {
+export default function TaskGridElement({ title, index, onClick }: PropTypes) {
   return (
     <div className="p-4 bg-gray-100 rounded-lg text-center">
-      <h1 className="font-bold mb-2">Aufgabe {task.id}</h1>
-      <p>{task.title}</p>
-      <Link to={`/tasks/${task.id}/firstNormalForm`}>
-        <button
-          type="button"
-          className="mt-4 px-4 py-1 bg-th-red hover:bg-red-600 text-white font-semibold rounded-md cursor-pointer"
-        >
-          Start
-        </button>
-      </Link>
+      <h1 className="font-bold mb-2">Aufgabe {index}</h1>
+      <p>{title}</p>
+      <button
+        type="button"
+        onClick={onClick}
+        className="mt-4 px-4 py-1 bg-th-red hover:bg-red-600 text-white font-semibold rounded-md cursor-pointer"
+      >
+        Start
+      </button>
     </div>
   )
 }
 
 interface PropTypes {
-  task: {
-    id: number
-    title: string
-  }
+  title: string
+  index: number
+  onClick: () => void
 }
