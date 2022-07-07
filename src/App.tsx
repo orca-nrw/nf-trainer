b7eca20ac9e49484ac44a127a8c624a0a2bc2542
Alexander Kosmehl <kosmehl@live.de>
2022-07-07 16:16:03 +0200

    Changed routing structure to match new router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import { Footer } from './Components/UI/Footer'
-import { Header } from './Components/UI/Header'
 import TrainerRouter from './Components/TrainerRouter'
+import { Route, Routes } from 'react-router-dom'
+import MainLayout from './Components/UI/MainLayout'
+import Home from './Components/Pages/Home'
 
 export default function App() {
   return (
     <div className="max-w-screen-xl space-y-4 mx-auto my-0 flex flex-col justify-center">
-      <Header />
-      <TrainerRouter />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="trainer/*" element={<TrainerRouter />} />
+        </Route>
+      </Routes>
     </div>
   )
 }
--- src/Components/TrainerRouter.tsx
+++ src/Components/TrainerRouter.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Task } from '../Types/Task'
+import { useFetch } from '../utils/useFetch'
 import CheckThirdNormalForm from './Pages/CheckThirdNormalForm'
 import Done from './Pages/Done'
 import FirstNormalForm from './Pages/FirstNormalForm'
 import FunctionalDependencies from './Pages/FunctionalDependencies'
 import FunctionalDependencyTypes from './Pages/FunctionalDependencyTypes'
-import Home from './Pages/Home'
 import PrimaryKeys from './Pages/PrimaryKeys'
 import SecondNormalForm from './Pages/SecondNormalForm'
 import TaskSelection from './Pages/TaskSelection'
@@ -13,53 +14,80 @@ import ThirdNormalForm from './Pages/ThirdNormalForm'
 import ViolatingColumns from './Pages/ViolatingColumns'
 
 export default function TrainerRouter() {
+  const [selectedTaskFileName, setSelectedTaskFileName] = useState('')
+  const { response: selectedTask, isLoading } = useFetch<Task>(
+    `./tasks/${selectedTaskFileName}.json`
+  )
+
   return (
-    <>
-      <div className="bg-white p-4">
-        <Link
-          className="no-underline text-black text-4xl font-semibold font-mono"
-          to="/"
-        >
-          NF-Trainer
-        </Link>
-      </div>
-      <div className="bg-white p-6">
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/tasks">
-            <TaskSelection />
-          </Route>
-          <Route path="/tasks/:id/firstNormalForm">
-            <FirstNormalForm />
-          </Route>
-          <Route path="/tasks/:id/violatingColumns">
-            <ViolatingColumns />
-          </Route>
-          <Route path="/tasks/:id/functionalDependencies">
-            <FunctionalDependencies />
-          </Route>
-          <Route path="/tasks/:id/primaryKeys">
-            <PrimaryKeys />
-          </Route>
-          <Route path="/tasks/:id/functionalDependencyTypes">
-            <FunctionalDependencyTypes />
-          </Route>
-          <Route path="/tasks/:id/secondNormalForm">
-            <SecondNormalForm />
-          </Route>
-          <Route path="/tasks/:id/checkThirdNormalForm">
-            <CheckThirdNormalForm />
-          </Route>
-          <Route path="/tasks/:id/thirdNormalForm">
-            <ThirdNormalForm />
-          </Route>
-          <Route path="/done">
-            <Done />
-          </Route>
-        </Switch>
-      </div>
-    </>
+    <Routes>
+      <Route
+        index
+        element={
+          <TaskSelection
+            selectedTaskFileName={selectedTaskFileName}
+            setSelectedTaskFileName={setSelectedTaskFileName}
+          />
+        }
+      />
+      <Route
+        path="/firstNormalForm"
+        element={
+          <FirstNormalForm selectedTask={selectedTask} isLoading={isLoading} />
+        }
+      />
+      <Route
+        path="/violatingColumns"
+        element={
+          <ViolatingColumns selectedTask={selectedTask} isLoading={isLoading} />
+        }
+      />
+      <Route
+        path="/functionalDependencies"
+        element={
+          <FunctionalDependencies
+            selectedTask={selectedTask}
+            isLoading={isLoading}
+          />
+        }
+      />
+      <Route
+        path="/primaryKeys"
+        element={
+          <PrimaryKeys selectedTask={selectedTask} isLoading={isLoading} />
+        }
+      />
+      <Route
+        path="/functionalDependencyTypes"
+        element={
+          <FunctionalDependencyTypes
+            selectedTask={selectedTask}
+            isLoading={isLoading}
+          />
+        }
+      />
+      <Route
+        path="/secondNormalForm"
+        element={
+          <SecondNormalForm selectedTask={selectedTask} isLoading={isLoading} />
+        }
+      />
+      <Route
+        path="/checkThirdNormalForm"
+        element={
+          <CheckThirdNormalForm
+            selectedTask={selectedTask}
+            isLoading={isLoading}
+          />
+        }
+      />
+      <Route
+        path="/thirdNormalForm"
+        element={
+          <ThirdNormalForm selectedTask={selectedTask} isLoading={isLoading} />
+        }
+      />
+      <Route path="/done" element={<Done />} />
+    </Routes>
   )
 }
