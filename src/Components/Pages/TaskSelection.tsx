c93f3d6ecc3f7dfdc992968a9189d15f95401eae
Alexander Kosmehl <kosmehl@live.de>
2022-07-07 16:18:26 +0200

    Updated selection page to work with fetched task list

--- src/Components/Pages/TaskSelection.tsx
+++ src/Components/Pages/TaskSelection.tsx
@@ -1,19 +1,64 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import TrainerHeader from '../UI/TrainerHeader'
 import TaskGridElement from '../UI/TaskGridElement'
-import tasks from '../../Tasks'
+import { useFetch } from '../../utils/useFetch'
+import { useNavigate } from 'react-router-dom'
 
-export default function TaskSelection() {
+interface Props {
+  selectedTaskFileName: string
+  setSelectedTaskFileName: (fileName: string) => void
+}
+
+interface TaskSettings {
+  selectedTasks: {
+    title: string
+    fileName: string
+  }[]
+}
+
+export default function TaskSelection({ selectedTaskFileName, setSelectedTaskFileName }: Props) {
+  const navigate = useNavigate()
+  const { response: taskSettings, isLoading } = useFetch<TaskSettings>(
+    './taskSettings.json'
+  )
+
+  function handleTaskSelection(fileName: string) {
+    setSelectedTaskFileName(fileName)
+  }
+
+  // Navigate once the filename has been set
+  useEffect(() => {
+    if (selectedTaskFileName) {
+      navigate('/trainer/firstNormalForm')
+    }
+  }, [selectedTaskFileName])
+
+  // Reset selectedFileName to avoid instant navigation
+  useEffect(() => {
+    setSelectedTaskFileName('')
+  }, [])
+
   return (
     <div className="space-y-8 mb-8">
       <TrainerHeader hideExplanationsModal={true}>
         Aufgabenauswahl
       </TrainerHeader>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 pt-4">
-        {tasks.map((task) => {
-          return <TaskGridElement key={task.id} task={task} />
-        })}
-      </div>
+      {isLoading || !taskSettings ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 pt-4">
+          {taskSettings.selectedTasks.map((task, idx) => {
+            return (
+              <TaskGridElement
+                key={idx}
+                title={task.title}
+                index={idx + 1}
+                onClick={() => handleTaskSelection(task.fileName)}
+              />
+            )
+          })}
+        </div>
+      )}
     </div>
   )
 }
