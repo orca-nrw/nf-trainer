4c299987a07ea7b7435aa1b1bb72d97acbd6278f
Alexander Kosmehl <kosmehl@live.de>
2022-07-08 14:55:25 +0200

    Removed duplicated styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,11 @@ import Home from './Components/Pages/Home'
 
 export default function App() {
   return (
-    <div className="max-w-screen-xl space-y-4 mx-auto my-0 flex flex-col justify-center">
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="trainer/*" element={<TrainerRouter />} />
-        </Route>
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="trainer/*" element={<TrainerRouter />} />
+      </Route>
+    </Routes>
   )
 }
