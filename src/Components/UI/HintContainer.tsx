23bb40e9fe2090c21bb94d8ab27a87a519081472
Alexander Kosmehl <kosmehl@live.de>
2022-07-08 15:28:49 +0200

    Added additional spacing on small devices

--- src/Components/UI/HintContainer.tsx
+++ src/Components/UI/HintContainer.tsx
@@ -14,11 +14,11 @@ export default function HintContainer({
 }: PropTypes) {
   return (
     <div className="flex justify-around">
-      <details className="px-3 py-1.5 bg-th-orange rounded-lg cursor-pointer select-none inline-block text-white font-semibold">
-        <summary className="font-semibold">Freigeschaltete Hinweise</summary>
-        <div className="cursor-default bg-white text-black font-normal p-2 mt-2 rounded-md">
+      <details className="bg-th-orange rounded-lg cursor-pointer select-none inline-block text-white font-semibold shadow-lg">
+        <summary className="font-semibold  px-3 py-2 ">Freigeschaltete Hinweise</summary>
+        <div className="cursor-default bg-white text-black font-normal p-2">
           {functionalDependencies && (
-            <div>
+            <div className='space-y-4 sm:space-y-0'>
               <h1 className="font-semibold">Funktionale Anforderungen</h1>
               {functionalDependencies?.map((dependency, index) => {
                 const dependencyString = `${dependency.primaryKeys.join(
@@ -30,7 +30,7 @@ export default function HintContainer({
           )}
 
           {primaryKeys && (
-            <div>
+            <div className='space-y-4 sm:space-y-0 mt-8'>
               <h1 className="font-semibold mt-4">Primärschlüssel</h1>
               <p>{primaryKeys.join(', ')}</p>
             </div>
